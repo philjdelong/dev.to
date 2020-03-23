90c84c2cd8ae0d5aac60938a62f8d1f764847e9d
Alison Vermeil <alison.minton@gmail.com>
2020-03-22 19:58:55 -0600

    Locate readingList pack and add notes

--- app/javascript/packs/readingList.jsx
+++ app/javascript/packs/readingList.jsx
@@ -1,14 +1,21 @@
 import { h, render } from 'preact';
 import { getUserDataAndCsrfToken } from '../chat/util';
+// Is this the connection of the user to the reading list? yes.
 import { ReadingList } from '../readingList/readingList';
 
 function loadElement() {
+  // CSRF cross site request forgery
+  // gets a curretUser object   .then returns a "promise" and when it resolves, the thing in the .then is what it resolves to
+  // so it returns a currentuser
   getUserDataAndCsrfToken().then(({ currentUser }) => {
+    // document means the entire UI of the site and grabs the reading-list id (element w/ the id of reading list)
     const root = document.getElementById('reading-list');
     if (root) {
       render(
+        // data is "passed down"
         <ReadingList
-          availableTags={currentUser.followed_tag_names}
+          // availableTags is a PROP
+          availableTags={currentUser.followed_tag_names} // so here's the currentUser's followed_tag_names
           statusView={root.dataset.view}
         />,
         root,
