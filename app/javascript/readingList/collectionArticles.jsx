ca4f312468d4c2ce3b9682e7bb172b59c230e7ae
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-28 17:00:26 -0600

    Update collectionArticles for header and styling

--- app/javascript/readingList/collectionArticles.jsx
+++ app/javascript/readingList/collectionArticles.jsx
@@ -2,15 +2,26 @@ import { h } from 'preact';
 import PropTypes from 'prop-types';
 import { Article } from './components/Article';
 
-export const CollectionArticles = collection => {
-  const articles = JSON.parse(collection.articles);
-  const articleList = articles.map(article => {
+export const CollectionArticles = ({ collection, articles }) => {
+  const collectionParse = JSON.parse(collection);
+  const articlesParse = JSON.parse(articles);
+  const articleList = articlesParse.map(article => {
     return <Article {...article} />;
   });
 
-  return <section className="collection item-wrapper">{articleList}</section>;
+  return (
+    <div className="items-container">
+      <section className="collection-cont results results--loaded">
+        <div className="results-header collection-header">
+          {`${collectionParse.name}`}
+        </div>
+        {articleList}
+      </section>
+    </div>
+  );
 };
 
 CollectionArticles.propTypes = {
   collection: PropTypes.string.isRequired,
+  articles: PropTypes.string.isRequired,
 };
