f62688edae59a01762738039e13a2a2d0aafd920
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-29 12:42:59 -0600

    Refactor collectionList to functional component

--- app/javascript/readingList/collectionList.jsx
+++ app/javascript/readingList/collectionList.jsx
@@ -1,47 +1,36 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import PropTypes from 'prop-types';
 import { Collection } from './components/Collection';
 
-export class CollectionList extends Component {
-  constructor({ collections }) {
-    super({ collections });
-
-    this.state = {
-      collections,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    const collectionsToRender = collections.map(collection => {
-      return (
-        <Collection
-          key={collection.id}
-          name={collection.name}
-          tags={collection.tag_list}
-          slug={collection.slug}
-        />
-      );
-    });
-
+export const CollectionList = ({ collections }) => {
+  const collectionsToRender = collections.map(collection => {
     return (
-      <section
-        className="collection-cont results results--loaded"
-        id="reading-collection"
-      >
-        <div className="results-header collection-header">
-          {collections.length
-            ? `Collections (${collections.length})`
-            : 'Collections'}
-          <a className="new-collection" href="/readingcollections/new">
-            +
-          </a>
-        </div>
-        {collectionsToRender}
-      </section>
+      <Collection
+        key={collection.id}
+        name={collection.name}
+        tags={collection.tag_list}
+        slug={collection.slug}
+      />
     );
-  }
-}
+  });
+
+  return (
+    <div
+      className="collection-cont results results--loaded"
+      id="reading-collection"
+    >
+      <div className="results-header collection-header">
+        {collections.length
+          ? `Collections (${collections.length})`
+          : 'Collections'}
+        <a className="new-collection" href="/readingcollections/new">
+          +
+        </a>
+      </div>
+      {collectionsToRender}
+    </div>
+  );
+};
 
 CollectionList.propTypes = {
   collections: PropTypes.arrayOf(PropTypes.object).isRequired,
