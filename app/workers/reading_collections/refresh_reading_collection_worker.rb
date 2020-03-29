83d48e7d328ec06617a58014ba52ba5baf25fb46
Alison Vermeil <alison.minton@gmail.com>
2020-03-29 10:06:09 -0600

    Update worker time for demo purposes

--- app/workers/reading_collections/refresh_reading_collection_worker.rb
+++ app/workers/reading_collections/refresh_reading_collection_worker.rb
@@ -5,7 +5,7 @@ module ReadingCollections
     sidekiq_options queue: :high_priority, retry: 10
 
     def perform
-      reading_collections = ReadingCollection.where("updated_at < ?", 10.seconds.ago)
+      reading_collections = ReadingCollection.where("updated_at < ?", 3.minutes.ago)
       reading_collections.each do |reading_collection|
         reading_collection.articles.clear
         articles = reading_collection.get_articles
