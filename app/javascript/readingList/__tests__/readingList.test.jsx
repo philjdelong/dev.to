822eeba22e25390d4300d609d19cbd29871bee5c
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-29 12:45:51 -0600

    Update tests to utilize new mocks

--- app/javascript/readingList/__tests__/readingList.test.jsx
+++ app/javascript/readingList/__tests__/readingList.test.jsx
@@ -1,15 +1,15 @@
 import { h } from 'preact';
 import render from 'preact-render-to-json';
 import { ReadingList } from '../readingList';
+import { mockCollectionList } from '../mocks/mockTestData';
 
 describe('<ReadingList />', () => {
   it('renders properly', () => {
-    const mockCollections = [
-      { id: 1, name: 'first_test_post' },
-      { id: 2, name: 'second_test_post' },
-    ];
     const tree = render(
-      <ReadingList availableTags={['discuss']} collections={mockCollections} />,
+      <ReadingList
+        availableTags={['discuss']}
+        collections={mockCollectionList}
+      />,
     );
     expect(tree).toMatchSnapshot();
   });
